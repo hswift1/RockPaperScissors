82ab887ac3e2bb7b5a6497b8cd496abf418e4ce7
SoftDev Student <softdevstudent@robotics-mba-06.local>
2019-01-18 15:21:35 -0500

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by SoftDev Student on 1/18/19.
+//  Copyright © 2019 Belle L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
